29f0b1789fc4cec562db66581c8f41cc400e90a9
Sylvain <sylvain.combraque@etudiant.univ-reims.fr>
2018-03-10 19:18:26 +0100

    Bugfix migrations

--- src/Migrations/Version20180310143646.php
+++ src/Migrations/Version20180310143646.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20180309120545 extends AbstractMigration
+class Version20180310143646 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
@@ -16,7 +16,7 @@ class Version20180309120545 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE `group` (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, parent_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, content LONGTEXT NOT NULL, last_update_ts DATETIME NOT NULL, INDEX IDX_6DC044C5A76ED395 (user_id), INDEX IDX_6DC044C5727ACA70 (parent_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE challenge (id INT AUTO_INCREMENT NOT NULL, challenge VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE challenge (id INT AUTO_INCREMENT NOT NULL, challenge VARCHAR(64) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE element (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, group_id INT DEFAULT NULL, content LONGTEXT NOT NULL, last_update_ts DATETIME NOT NULL, INDEX IDX_41405E39A76ED395 (user_id), INDEX IDX_41405E39FE54D947 (group_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE token (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, token VARCHAR(255) NOT NULL, machine_name VARCHAR(255) NOT NULL, ip VARCHAR(255) NOT NULL, login_ts DATETIME NOT NULL, last_update_ts DATETIME NOT NULL, INDEX IDX_5F37A13BA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, is_admin TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
